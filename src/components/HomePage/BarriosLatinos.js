2cd67e2cecc1c8402196802f38904fc5e4dda79c
Dylan Ocampo <dylanomar@live.com.mx>
2026-04-08 11:51:27 -0600

    encuesta

--- src/components/HomePage/BarriosLatinos.js
+++ src/components/HomePage/BarriosLatinos.js
@@ -4,6 +4,9 @@ import splashArt from "../../assets/main/IMG_Splash_1Home.webp";
 import splashArtDesktop from "../../assets/desktop/Main/IMG_Splash_1Home_web.webp";
 import { useViewport } from "../../context/ViewportContext";
 
+import fondo from "../../assets/main/Button_Registro al torneo.webp";
+import highlight from "../../assets/desktop/Header/Spray_graffbutton.webp"
+
 const BarriosLatinos = () => {
   const { isMobile } = useViewport();
 
@@ -16,6 +19,23 @@ const BarriosLatinos = () => {
     >
       <img src={splash} style={{marginTop: "80px"}} alt="Splash Art" />
       
+      
+      <div
+        onClick={() => { window.gtag?.('event', 'click_participar_encuesta', { event_category: 'engagement', event_label: 'floating_button' }); window.open("https://forms.gle/QGZghRzUyTB2kyHz6", "_blank"); }}
+        className="survey-container"
+        style={{
+          opacity: 1,
+          pointerEvents: 'auto',
+          transition: 'opacity 0.3s ease',
+          cursor: "pointer"
+        }}
+      >
+        <img loading="lazy" src={fondo} alt="img" className="survey-image"/>
+        <img loading="lazy" src={highlight} alt="" className="survey-highlight"/>
+        <p className="guild-font survey-text">REGÍSTRATE AHORA</p>
+      </div>
+
+
 
     </section>
   );
